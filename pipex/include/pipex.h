f0793d7eb4d8453e5d661ec7e0be9f61788f84bc
CABARBAYE Adrien <cabarbaye.adrien@gmail.com>
2024-04-10 15:48:45 +0200

    initialisation du projet

---
+++ pipex/include/pipex.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acabarba <acabarba@student.42Perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 15:43:57 by acabarba          #+#    #+#             */
+/*   Updated: 2024/04/10 15:43:57 by acabarba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+
+#endif
