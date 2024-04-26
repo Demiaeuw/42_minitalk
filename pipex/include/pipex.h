42e27567795a6cbeb13cbda2016b33812615832b
cabarbaye <cabarbaye.adrien@gmail.com>
2024-04-27 00:12:16 +0200

    init du projet

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -1,21 +1,20 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: acabarba <acabarba@student.42Perpignan.    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/10 15:43:57 by acabarba          #+#    #+#             */
-/*   Updated: 2024/04/10 15:43:57 by acabarba         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef PIPEX_H
 # define PIPEX_H
 # include <unistd.h>
-# include <stdarg.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <stdarg.h>
+# include <fcntl.h>
+# include <string.h>
+# include <stdarg.h>
+# include <fcntl.h>
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include "libft.h"
 
+
+int	main(void);
+
+/*			UTILS			*/
 
 #endif
